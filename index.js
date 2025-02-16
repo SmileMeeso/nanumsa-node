335531faf1ce7ad2cd6fb39b43b683d5c89837f2
SmileMeeso <m950827@naver.com>
2025-02-16 17:48:11 +0900

    socket 활성화

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import { Server } from "socket.io";
+import { createServer } from "http";
+import express from "express";
+import morgan from "morgan";
 
-const port = 3000;
-const io = new Server(port, {
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
     cors: {
-        origin: ["https://www.nanumsa.com"],
+        origin: ["https://www.nanumsa.com", "172.31.3.54"],
         methods: ["GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"],
-        credentials: true,
+        credentials: false,
         transports: ["websocket"],
         allowedHeaders: ["*"],
     },
     transports: ["websocket"],
 });
 
+app.use(morgan("combined"));
+
 const clients = {};
 
 io.on("connection", (socket) => {
+    console.log("connection established.");
+
     socket.on("message", (token) => {
         if (!clients[token]) {
             console.log("new client", token);
@@ -23,4 +32,10 @@ io.on("connection", (socket) => {
             clients[token].emit("message", token);
         }
     });
+});
+
+// 포트 번호와 서버 시작
+const PORT = process.env.PORT || 3000; // 원하는 포트 번호
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
